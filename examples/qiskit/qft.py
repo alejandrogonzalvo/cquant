14c1ed9c3f5b518e554c563d691a6499039baa5f
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2024-02-18 08:22:38 +0100

    generalize qft script

--- examples/qiskit/qft.py
+++ examples/qiskit/qft.py
@@ -1,7 +1,15 @@
 from qiskit import QuantumCircuit, qasm3
 from qiskit.circuit.library import QFT
+from sys import argv
 
-circuit: QuantumCircuit = QFT(40)
+if len(argv) != 2:
+    print("Usage: python qft.py <number of qubits>")
+    exit(1)
+
+
+circuit: QuantumCircuit = QFT(int(argv[1]))
 
 with open("../input/qft.qasm", "w") as f:
+    content: str = qasm3.dumps(circuit)
+    content.replace("stdgates", "mygates", 1)
     f.write(qasm3.dumps(circuit))
